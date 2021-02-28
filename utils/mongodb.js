cf73f2e89e148c038d5002d6b2870f06ac77aad0
llenaX <jaimitr14@gmail.com>
2021-03-01 02:30:17 +0300

    mongo

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 const connection = {};
 
+const URI =
+  "mongodb+srv://dbLenax:" +
+  process.env.DB_PASSWORD +
+  "@lnx.1rdhz.mongodb.net/lnx?retryWrites=true&w=majority";
+
 const dbConnect = async () => {
   if (!connection.isConnected) {
-    const db = await mongoose.connect(process.env.DB_URI, {
+    const db = await mongoose.connect(URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
